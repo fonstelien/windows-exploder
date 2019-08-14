59d3ce1512b0a021593f425bf5b3da9290b5bf1c
fonstelien <olav.fonstelien@gmail.com>
2019-08-14 23:37:49 +0200

    set max widt, height, dynamically

--- dropdown.py
+++ dropdown.py
@@ -1,54 +1,59 @@
 #!/usr/bin/env python3
 
-import subprocess
 import urwid
 import editor
 import walker
 
 
 class DropDown(urwid.Frame):
-    signals = ['close']
+    signals = ['close', 'render']
 
     def __init__(self):
         self.editor = editor.Editor()
-        self.msg_buffer = list()  # list object for message from Walker object
-        self.walker = walker.Walker(msg_buffer=self.msg_buffer)
-        super(DropDown, self).__init__(header=self.editor, body=self.walker,
-                                       focus_part='header')
-        self.full_range_list = list()  # The full range of autocomplete options
-        self.full_range_string = ""    # range as '\n'.join(full_range_list)
-
-    def _set_walker_contents(self):
-        if self.editor.edit_text == "":
-            return self.walker.show(self.full_range_list)
-        subproc = subprocess.run(
-            f"echo \"{self.full_range_string}\" | \
-            grep {self.editor.edit_text}",
-            shell=True, capture_output=True, encoding='UTF-8')
-        self.walker.show(subproc.stdout.splitlines())
+        self.walker = walker.Walker(focus_attr='dropdown_walk')
+        super(DropDown, self).__init__(
+            header=self.editor, focus_part='header',
+            body=urwid.AttrMap(self.walker, attr_map='dropdown_plain'))
+        # The longest string in the options range. Updated in init_new_word()
+        self.max_width = 0
+        self.selection = ''  # The selected option returned to handling widget
 
     def init_new_word(self, search_str, full_range_list):
         self.editor.edit_text = search_str
         self.editor.edit_pos = len(search_str)
-        self.full_range_list = full_range_list
-        self.full_range_string = '\n'.join(full_range_list)
-        self._set_walker_contents()
+        self.walker.set_content(full_range_list)
+        self.max_width = max(map(len, full_range_list))
+        self.walker.filter_content(self.editor.edit_text,
+                                   attr_marked='dropdown_marked',
+                                   attr_plain='dropdown_plain')
+
+    @property
+    def curr_height(self):
+        """Total number of rows in the pop up, incl. 1 row for the editor."""
+        return self.walker.length() + 1
 
     def keypress(self, size, key):
+        # This lets the user still modify the edit_text when focus is on 'body'
+        if len(key) == 1 or key in self.editor._deleters:
+            self.set_focus('header')
+            super(DropDown, self).keypress(size, key)
+            self.walker.filter_content(self.editor.edit_text,
+                                       attr_marked='dropdown_marked',
+                                       attr_plain='dropdown_plain')
+            self._emit('render')
+            return key
+
+        # Actions to complete after sending key to underlaying widget
         super(DropDown, self).keypress(size, key)
 
-        if key == 'down' and self.focus is self.editor:
+        if key == 'down' and self.get_focus() == 'header':
             self.set_focus('body')
 
-        elif key in ('enter', 'tab') and self.focus is self.walker:
-            self.walker.set_message()
-            self.editor.edit_text = self.msg_buffer[0]
+        elif key in ('enter', 'tab') and self.get_focus() == 'body':
+            self.editor.edit_text = self.walker.get_selected_message()
             self.editor.edit_pos = len(self.editor.edit_text)
-            self.msg_buffer.clear()
+            self.selection = self.walker.get_selected_message()
             self.set_focus('header')
             self._emit('close')
-
-        elif len(key) == 1 or key in self.editor._deleters:
-            self._set_walker_contents()
 
         return key
