2ddf49bff9671fb0694da61bb12461a66214d98e
fonstelien <olav.fonstelien@gmail.com>
2019-08-18 00:26:46 +0200

    selectable() returns Walker.selectable()

--- dropdown.py
+++ dropdown.py
@@ -12,25 +12,37 @@ class DropDown(urwid.Frame):
         self.editor = editor.Editor()
         self.walker = walker.Walker(focus_attr='dropdown_walk')
         super(DropDown, self).__init__(
-            header=self.editor, focus_part='header',
-            body=urwid.AttrMap(self.walker, attr_map='dropdown_plain'))
-        # The longest string in the options range. Updated in init_new_word()
-        self.max_width = 0
+            header=urwid.AttrMap(self.editor, attr_map='dropdown_editor'),
+            body=urwid.AttrMap(self.walker, attr_map='dropdown_plain'),
+            focus_part='header')
+        # The longest string in the options range. Updated in set_content()
+        self.max_width = 1
         self.selection = ''  # The selected option returned to handling widget
+        self._selectable = False
 
-    def init_new_word(self, search_str, full_range_list):
-        self.editor.edit_text = search_str
-        self.editor.edit_pos = len(search_str)
-        self.walker.set_content(full_range_list)
-        self.max_width = max(map(len, full_range_list))
+    def set_content(self, content_list):
+        self.walker.set_content(content_list)
+        self._selectable = self.walker.selectable()
+        if self._selectable:
+            # +1 for the cursor
+            self.max_width = max(map(len, content_list)) + 1
+
+    def set_edit_text(self, edit_text):
+        self.editor.edit_text = edit_text
+        self.editor.edit_pos = len(edit_text)
         self.walker.filter_content(self.editor.edit_text,
                                    attr_marked='dropdown_marked',
                                    attr_plain='dropdown_plain')
 
+    def has_match(self, search_str):
+        if search_str == '':
+            return False
+        return self.walker.has_match(search_str)
+
     @property
     def curr_height(self):
-        """Total number of rows in the pop up, incl. 1 row for the editor."""
-        return self.walker.length() + 1
+        """Total number of rows in the pop up,"""
+        return self.walker.length() + 1  # Add 1 row for the edit line
 
     def keypress(self, size, key):
         # This lets the user still modify the edit_text when focus is on 'body'
@@ -40,6 +52,13 @@ class DropDown(urwid.Frame):
             self.walker.filter_content(self.editor.edit_text,
                                        attr_marked='dropdown_marked',
                                        attr_plain='dropdown_plain')
+
+            # Signal 'close' and return edit_text to calling widget
+            if not self.has_match(self.editor.edit_text):
+                self.selection = self.editor.edit_text
+                self._emit('close')
+                return key
+
             self._emit('render')
             return key
 
@@ -49,9 +68,12 @@ class DropDown(urwid.Frame):
         if key == 'down' and self.get_focus() == 'header':
             self.set_focus('body')
 
-        elif key in ('enter', 'tab') and self.get_focus() == 'body':
-            self.editor.edit_text = self.walker.get_selected_message()
-            self.editor.edit_pos = len(self.editor.edit_text)
+        # Signal 'close' and return selection to calling widget
+        elif key in ('enter', 'tab'):
+            # Set focus to body in order to select the first entry when an
+            # active selection has not been made
+            self.set_focus('body')
+
             self.selection = self.walker.get_selected_message()
             self.set_focus('header')
             self._emit('close')
