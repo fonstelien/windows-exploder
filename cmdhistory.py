5dcdd739749fbee1ed921c5493e95b2b0aac5118
fonstelien <olav.fonstelien@gmail.com>
2019-08-24 17:06:38 +0200

    clean up CmdHistory.next()

--- cmdhistory.py
+++ cmdhistory.py
@@ -1,7 +1,6 @@
 import urwid
 import copy
 
-from programstatus import ProgramStatus
 
 class CmdHistory(object):
     def __init__(self, program_status):
@@ -10,7 +9,7 @@ class CmdHistory(object):
         self.idx = 0
         self.search_str = ""
         self.add(program_status)
-        
+
     def add(self, program_status):
         for item in self.history:
             if vars(program_status) == vars(item):
@@ -18,31 +17,25 @@ class CmdHistory(object):
         self.history.append(copy.copy(program_status))
         self.length = len(self.history)
         self.reset()
-        
+
     def reset(self, search_str=""):
         self.idx = self.length - 1
         self.search_str = search_str
-        
+
     def next(self, step, _recur_idx=0):
         if _recur_idx == self.length:
             return None
-        
-        self.idx = max(0, (self.idx + step) % self.length)
-        item = None
-        try:
-            item = self.history[self.idx]
-        except IndexError as e:
-            raise urwid.ExitMainLoop(self.idx)
 
-            
+        self.idx = max(0, (self.idx + step) % self.length)
+        item = self.history[self.idx]
         if self.search_str in item.command:
             return item
         return self.next(step, _recur_idx+1)
 
     def last(self):
         return self.history[-1]
-    
-    
+
+
 class CmdHistoryWidget(urwid.Edit):
     def __init__(self, program_status):
         self.program_status = program_status
@@ -51,7 +44,7 @@ class CmdHistoryWidget(urwid.Edit):
 
     def add(self):
         self.history.add(self.program_status)
-        
+
     def reset_widget(self):
         self.edit_text = ""
         self.history.reset()
@@ -66,8 +59,8 @@ class CmdHistoryWidget(urwid.Edit):
             if self.edit_text == "":
                 self.history.reset(search_str="")
                 self.program_status.copy_state(self.history.last())
-                
-            # Reset history with new search_str and copy state to program_status
+
+            # Set new search_str and copy state to program_status
             elif self.edit_text not in self.program_status.command:
                 self.history.reset(search_str=self.edit_text)
                 state = self.history.next(-1)
@@ -83,7 +76,7 @@ class CmdHistoryWidget(urwid.Edit):
 
         elif key == 'down':
             self.program_status.copy_state(self.history.next(+1))
-         
+
         elif key == 'esc':
             self.program_status.copy_state(self.history.last())
 
@@ -91,11 +84,10 @@ class CmdHistoryWidget(urwid.Edit):
         #     last_state = self.history.last()
         #     mode_id = last_state.mode_id
         #     self.program_status.set_result()
-            
+
         elif key == 'tab':
             pass
-            # if self.program_status.cwd != os.getcwd():
-                
+            # self.program_status.copy_state(self.history.last())
 
         if key in ('esc', 'enter', 'tab'):
             self.reset_widget()
